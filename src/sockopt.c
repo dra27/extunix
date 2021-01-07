25ee2054fea1b36a0b9175fce1842c21f30a53ac
Antonin Décimo <antonin@tarides.com>
2021-01-07 18:59:57 +0100

    Fix invalid pointer type on Windows
    On Windows, optval is char *; on Unix it is void *.
    Casting to void * should satisfy both worlds.

--- src/sockopt.c
+++ src/sockopt.c
@@ -86,7 +86,7 @@ CAMLprim value caml_extunix_setsockopt_int(value fd, value k, value v)
     assert(0);
   }
 
-  if (0 != setsockopt(Int_val(fd), tcp_options[Int_val(k)].level, tcp_options[Int_val(k)].opt, &optval, optlen))
+  if (0 != setsockopt(Int_val(fd), tcp_options[Int_val(k)].level, tcp_options[Int_val(k)].opt, (void *)&optval, optlen))
   {
 #ifdef _WIN32
     win32_maperr(WSAGetLastError());
@@ -117,7 +117,7 @@ CAMLprim value caml_extunix_getsockopt_int(value fd, value k)
     assert(0);
   }
 
-  if (0 != getsockopt(Int_val(fd), tcp_options[Int_val(k)].level, tcp_options[Int_val(k)].opt, &optval, &optlen))
+  if (0 != getsockopt(Int_val(fd), tcp_options[Int_val(k)].level, tcp_options[Int_val(k)].opt, (void *)&optval, &optlen))
   {
 #ifdef _WIN32
     win32_maperr(WSAGetLastError());
